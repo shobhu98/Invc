46f458bab95e6086378d192b0b924f4ea82d2c32
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2019-12-04 14:56:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,48 @@ let app = express();
 app.set('view engine', 'ejs');
 let path=require('path');
 let nodemailer=require('nodemailer');
-// let twilio=require('twilio');
 
-const accountSid = 'AC973e8decd35333060224d07568f3e4e6';
-const authToken = 'ce0a17c21503a4e8a2185e6965b00ced';
-const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//     .create({
-//         body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//         from: '+19543728578',
-//         to: '+918920862975'
-//     })
-//     .then(message => console.log(message.sid));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static( 'public'));
+
+const accountSid = 'ACcb5e785191c3241e9e8e55df6d4a897e';
+const authToken = '5e775cf6de4d91a11dadb1c526e283fa';
+const client = require('twilio')(accountSid, authToken);
+
+
+
+
+const Nexmo = require('nexmo');
+
+const nexmo = new Nexmo({
+    apiKey: '606d23b2',
+    apiSecret: 'IHbO2p6vlY2hBjb8',
+
+});
+// nexmo.message.sendSms('NEXMO','+918920862975', "hello everyone",function (err,data) {
+//     if(err){
+//         console.log(err)
+//     }
+//     else {
+//         console.log(data);
+//     }
+//
+// });
+
+
+
+
+client.messages
+    .create({
+        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+        from:'+19543728578',
+        to: '+918920862975',
+    })
+    .then(message => console.log(message.sid));
+
+
 
 
 
@@ -91,13 +116,24 @@ let maillist=[email_host];
     }
   });
 
-    client.messages
-        .create({
-            body: "Email_id: "+ email_str+ "\n Name: "+name_str+"\n Number: "+num_str+"\nIn Time: "+time_in,
-            from: '+19543728578',
-            to: '+91'+num_host
-        })
-        .then(message => console.log(message.sid));
+    // client.messages
+    //     .create({
+    //         body: "Email_id: "+ email_str+ "\n Name: "+name_str+"\n Number: "+num_str+"\nIn Time: "+time_in,
+    //         from: '+19546271845',
+    //         to: '+91'+num_host
+    //     })
+    //     .then(message => console.log(message.sid));
+
+
+    nexmo.message.sendSms('NEXMO','+91'+num_host, "Email_id: "+ email_str+ "\n Name: "+name_str+"\n Number: "+num_str+"\nIn Time: "+time_in,function (err,data) {
+        if(err){
+            console.log(err)
+        }
+        else {
+            console.log(data);
+        }
+
+    });
 
 
 
@@ -120,13 +156,26 @@ app.get('/thankyou',function (req,res,next) {
   };
          console.log(num);
          console.log(typeof (num));
-    client.messages
-        .create({
-            body: 'Thanks For visiting Mr/Ms '+name_host+" at Inovaccer.\n"+"Email: "+ email_str+ "\nName: "+name_str+"\nNumber "+num_str+"\nTime In "+time_in+"\nTime Out "+time_out,
-            from: '+19543728578',
-            to: '+91'+num
-        })
-        .then(message => console.log(message.sid));
+    // client.messages
+    //     .create({
+    //         body: 'Thanks For visiting Mr/Ms '+name_host+" at Inovaccer.\n"+"Email: "+ email_str+ "\nName: "+name_str+"\nNumber "+num_str+"\nTime In "+time_in+"\nTime Out "+time_out,
+    //         from: '+19546271845',
+    //         to: '+91'+num
+    //     })
+    //     .then(message => console.log(message.sid));
+
+
+    nexmo.message.sendSms('NEXMO','+91'+num,'Thanks For visiting Mr/Ms '+name_host+" at Inovaccer.\n"+"Email: "+ email_str+ "\nName: "+name_str+"\nNumber "+num_str+"\nTime In "+time_in+"\nTime Out "+time_out,function (err,data) {
+        if(err){
+            console.log(err)
+        }
+        else {
+            console.log(data);
+        }
+
+    });
+
+
 
 
   transporter.sendMail(mailOptions, function(error, info){
